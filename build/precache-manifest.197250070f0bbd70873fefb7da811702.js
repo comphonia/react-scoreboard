7677e2be182c79125653931c856eab82613fb881
comphonia <online.comphonia@gmail.com>
2019-06-07 04:51:00 -0500

    build

---
+++ build/precache-manifest.197250070f0bbd70873fefb7da811702.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae509e66519d9782216c6c373070c91",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "aba0a8ac3d9287a096d7",
+    "url": "/react-scoreboard/static/css/main.140df8a8.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/react-scoreboard/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "aba0a8ac3d9287a096d7",
+    "url": "/react-scoreboard/static/js/main.42764436.chunk.js"
+  },
+  {
+    "revision": "da4941b842451d92ab2c",
+    "url": "/react-scoreboard/static/js/runtime~main.d9728706.js"
+  }
+]);
