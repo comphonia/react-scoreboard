86a3513f5a8575aeee733c190b85b3d9c4cebd0d
comphonia <online.comphonia@gmail.com>
2019-06-07 05:05:56 -0500

    build again

---
+++ build/precache-manifest.38b60106074a703012053d9006c02017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1764add4057d725875ae1fd3fba516c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aba0a8ac3d9287a096d7",
+    "url": "./static/css/main.140df8a8.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "./static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "aba0a8ac3d9287a096d7",
+    "url": "./static/js/main.42764436.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-scoreboard/precache-manifest.197250070f0bbd70873fefb7da811702.js"
+  "./precache-manifest.38b60106074a703012053d9006c02017.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-scoreboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
